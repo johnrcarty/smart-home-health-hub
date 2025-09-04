7003c048dd24f65010653f60b4d6698b25944093
John <johnrcarty@icloud.com>
2025-09-04 00:55:05 -0400

    refractor

--- backend/mqtt/__init__.py
+++ backend/mqtt/__init__.py
@@ -4,6 +4,7 @@ from .publisher import MQTTPublisher
 from .discovery import send_mqtt_discovery
 from .handlers import MQTTMessageHandlers, create_message_handlers
 from .settings import get_mqtt_settings, is_mqtt_enabled, get_vital_topic_config
+from .service import initialize_mqtt_service, shutdown_mqtt_service, get_mqtt_service
 
 __all__ = [
     'get_mqtt_client', 
@@ -14,5 +15,8 @@ __all__ = [
     'create_message_handlers',
     'get_mqtt_settings',
     'is_mqtt_enabled',
-    'get_vital_topic_config'
+    'get_vital_topic_config',
+    'initialize_mqtt_service',
+    'shutdown_mqtt_service',
+    'get_mqtt_service'
 ]
